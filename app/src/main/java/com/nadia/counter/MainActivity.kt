0e5dd6083215d4ab622e7d2c60fc85e9c4fbb8bb
Nadia Hassouni <hassouninadia20@gmail.com>
2021-12-22 18:21:52 +0100

    counter mainActivity

---
+++ app/src/main/java/com/nadia/counter/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.nadia.counter
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nadia.counter.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private var counter = 0
+    private val colorsTxt: Array<String> = applicationContext.resources.getStringArray(R.array.colors)
+    private val colors : MutableList<Int> ?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.layout.setOnClickListener {
+            increaseCounterChangeColor(getRandomColor())
+        }
+        binding.ivReset.setOnClickListener {
+            counter = 0
+        }
+    }
+
+    private fun getRandomColor(): Int {
+        for (i in 0..colorsTxt.size) {
+            val newColor = Color.parseColor(colorsTxt[i])
+            colors?.add(newColor)
+        }
+        return colors!![Random.nextInt(colors.size)]
+    }
+
+    private fun increaseCounterChangeColor(color : Int) {
+        counter++
+        binding.tvCounter.text = counter.toString()
+        binding.layout.setBackgroundColor(color)
+    }
+
+}
