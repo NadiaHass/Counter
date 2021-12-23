39d6697823fb8fb565b6112b3a9abaf215c841e7
Nadia Hassouni <hassouninadia20@gmail.com>
2021-12-23 22:13:41 +0100

    increase counter

---
+++ app/src/main/java/com/nadia/counterapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.nadia.counterapp
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    lateinit var counterTextView: TextView
+    private var counter = 0
+    private val colorsTxt: Array<String> = arrayOf(
+        "#F37F7F",
+        "#94D3A5",
+        "#856CCA",
+        "#FF5722",
+        "#CDDC39",
+        "#009688",
+        "#4CAF50",
+        "#615F5F",
+        "#DBF3B6",
+        "#BFFFE9")
+    private val colors: MutableList<Int>? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        counterTextView = findViewById(R.id.tv_counter)
+        val resetButton = findViewById<ImageView>(R.id.iv_reset)
+        val layout = findViewById<ConstraintLayout>(R.id.layout)
+
+        layout.setOnClickListener {
+            increaseCounterChangeColor()
+        }
+        resetButton.setOnClickListener {
+            counter = 0
+            counterTextView.text = counter.toString()
+            counterTextView.setTextColor(Color.BLACK)
+        }
+    }
+
+    private fun getRandomColor(): Int {
+        for (i in 0..colorsTxt.size) {
+            colors?.add(Color.parseColor(colorsTxt[i]))
+        }
+        return colors!![Random.nextInt(colors.size)]
+    }
+
+    private fun increaseCounterChangeColor() {
+        counterTextView.text = counter.toString()
+        counter++
+        counterTextView.text = counter.toString()
+        if (counter % 10 == 0) {
+            counterTextView.setTextColor(getRandomColor())
+        }
+    }
+}
